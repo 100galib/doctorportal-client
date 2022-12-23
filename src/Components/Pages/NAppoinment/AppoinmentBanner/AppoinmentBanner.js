69334a04c45ff0fcf7ccc16a35ca14b1bffb44e9
Ahammed Fahim Galib <galib0124@gmail.com>
2022-12-24 00:21:26 +0600

    day picker added

--- src/Components/Pages/NAppoinment/AppoinmentBanner/AppoinmentBanner.js
+++ src/Components/Pages/NAppoinment/AppoinmentBanner/AppoinmentBanner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import bgImage from '../../../../assets/images/bg.png';
 import rightImage from '../../../../assets/images/chair.png';
+import { DayPicker } from 'react-day-picker';
 
 const AppoinmentBanner = () => {
     return (
@@ -9,9 +10,7 @@ const AppoinmentBanner = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={rightImage} className="shadow-2xl lg:max-w-xl" alt='banner area'/>
                     <div className='mt-20 lg:mt-10'>
-                    <h1 className="text-6xl lg:text-5xl font-bold">Your New Smile Starts Here</h1>
-                    <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
-                    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Get Started</button>
+                        <DayPicker></DayPicker>
                     </div>
                 </div>
             </div>
