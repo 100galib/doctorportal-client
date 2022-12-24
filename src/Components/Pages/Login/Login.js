1759bc1118d32038ff386e76808a81601a15aa96
Ahammed Fahim Galib <galib0124@gmail.com>
2022-12-25 00:00:47 +0600

    registration complete

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,31 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const { register, handleSubmit } = useForm();
-    const [data, setData] = useState("");
+
+    const handleLogin = data => {
+        console.log(data)
+    }
 
     return (
-        <div className="h-96 flex justify-center items-center">
+        <div className="h-96 flex justify-center items-center my-10">
             <div className='shadow-xl w-96 p-7'>
-                <h1 className='text-center text-3xl'>LogIn</h1>
-                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                <h1 className='text-center text-3xl font-bold'>LogIn</h1>
+                <form onSubmit={handleSubmit(handleLogin)}>
                     <div className="form-control w-full">
                         <label className="label"> 
                             <span className="label-text font-bold">Email</span>
                         </label>
-                        <input  {...register("name")} type="text" placeholder="Email" className="input input-bordered w-full" />
+                        <input  {...register("email")} type="text" className="input input-bordered w-full" />
                     </div>
                     <div className="form-control w-full">
                         <label className="label"> 
                             <span className="label-text font-bold">Password</span>
                         </label>
-                        <input  {...register("password")} type="password" placeholder="Password" className="input input-bordered w-full" />
+                        <input  {...register("password")} type="password" className="input input-bordered w-full" />
                     </div>
-                    <br />
-                    <p>{data}</p>
+                    <label className="label"> 
+                            <span className="label-text">Forgot Password</span>
+                    </label>
                     <input className='btn w-full' type="submit" value={'Login'}/>
                 </form>
+                <p className='text-xs text-center mt-3'>New To Doctor Portal ? <Link to={'/registration'} className='text-secondary'>Create new account</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
             </div>
         </div>
     );
--- src/Components/Pages/Registration/Registration.js
+++ src/Components/Pages/Registration/Registration.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Registration = () => {
+    const { register, handleSubmit } = useForm();
     return (
-        <div>
-            <h1>This is Registration</h1>
+        <div className="h-96 flex justify-center items-center my-28">
+            <div className='shadow-xl w-96 p-7'>
+                <h1 className='text-center text-3xl font-bold'>Registration</h1>
+                <form onSubmit={handleSubmit()}>
+                    <div className="form-control w-full">
+                        <label className="label"> 
+                            <span className="label-text font-bold">Name</span>
+                        </label>
+                        <input  {...register("name")} type="text" className="input input-bordered w-full" />
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label"> 
+                            <span className="label-text font-bold">Email</span>
+                        </label>
+                        <input  {...register("email")} type="email" className="input input-bordered w-full" />
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label"> 
+                            <span className="label-text font-bold">Password</span>
+                        </label>
+                        <input  {...register("password")} type="password" className="input input-bordered w-full" />
+                    </div>
+                    <input className='btn w-full mt-5' type="submit" value={'Signup'}/>
+                </form>
+                <p className='text-xs text-center mt-3'>Already Have an Account ? <Link to={'/login'} className='text-secondary'>logIn</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
